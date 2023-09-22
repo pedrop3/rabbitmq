aaa7d70e2116869ee4f53080c6cc65d76b2d9f8f
Pedro Santos <pedro_henriquems321@hotmail.com>
2023-09-22 01:21:08 -0300

    init

---
+++ src/main/java/br/com/learn/rabbitmq/RabbitmqApplication.java
@@ -0,0 +1,15 @@
+package br.com.learn.rabbitmq;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableRabbit
+@SpringBootApplication
+public class RabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqApplication.class, args);
+	}
+
+}
