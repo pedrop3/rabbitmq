317674e59b91b5a658c7ff6a08ea82d243d3ff84
Pedro Santos <pedro_henriquems321@hotmail.com>
2023-10-12 14:28:56 -0300

    created bean exchange

---
+++ src/main/java/br/com/learn/rabbitmq/config/RabbitMQConfig.java
@@ -0,0 +1,58 @@
+package br.com.learn.rabbitmq.config;
+
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.support.RetryTemplate;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public DirectExchange directExchange() {
+        return new DirectExchange("event.v1.created.ex");
+    }
+
+    @Bean
+    public DirectExchange directExchangeDLX() {
+        return new DirectExchange("event.v1.created.dlx");
+    }
+
+    public RetryTemplate retryTemplate() {
+        RetryTemplate RetryTemplate = new RetryTemplate();
+        return new RetryTemplate();
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
+        rabbitAdmin.setRetryTemplate(retryTemplate());
+        return rabbitAdmin;
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> applicationReadyEventApplicationListener(
+            RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+            Jackson2JsonMessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+
+}
